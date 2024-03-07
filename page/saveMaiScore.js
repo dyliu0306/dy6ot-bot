163cade8ad56048d3831dcb013a5a7cf2225c63a
dyliu0306 <89912899+dyliu0306@users.noreply.github.com>
2024-03-07 17:14:02 +0800

    Create saveMaiScore.js

---
+++ page/saveMaiScore.js
@@ -0,0 +1,10 @@
+const json = JSON.stringify(temp1, null, 2);
+const blob = new Blob([json], { type: "application/json" });
+const url = URL.createObjectURL(blob);
+const a = document.createElement("a");
+a.href = url;
+a.download = "score.json";
+document.body.appendChild(a);
+a.click();
+document.body.removeChild(a);
+URL.revokeObjectURL(url);
